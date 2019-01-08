2ab18b1dd0b9768c284637eba7b998c3db410a52
Daniel Smith <danielsmithwba@Daniels-MacBook-Air-2.local>
2019-01-08 15:46:57 +0000

    updates

--- 405 assignment 2/Code/sketch.js
+++ 405 assignment 2/Code/sketch.js
@@ -41,20 +41,21 @@ function mercY(lat) {
 
 function setup() {
   createCanvas(ww,hh);//creating a canvas
-  translate(width / 2, height / 2)
-  imageMode(CENTER);
+  translate(width / 2, height / 2)//moving the canvas to the middle
+  imageMode(CENTER);// changes where the image is drawn from
   image(mapimg, 0, 0);
 
     var cx = mercX(clon);
     var cy = mercY(clat);
 
+//looping the arrays and splitting up the data into more manageable variables
     for (var i = 1; i < earthquakes.length; i++) {
       var data = earthquakes[i].split(/,/);
       console.log(data);
       var lat = data[1];
       var lon = data[2];
       var mag = data[4];
-      var x = mercX(lon) - cx;
+      var x = mercX(lon) - cx;// converts it to an x and y so can draw an ellipse
       var y = mercY(lat) - cy;
       // This addition fixes the case where the longitude is non-zero and
       // points can go off the screen.
@@ -63,14 +64,14 @@ function setup() {
       } else if(x > width / 2) {
         x -= width;
       }
-      mag = pow(10, mag);
-      mag = sqrt(mag);
-      var magmax = sqrt(pow(10, 10));
-      var d = map(mag, 0, magmax, 0, 180);
-      stroke(255, 0, 255);
-      fill(random(100,255), random(100,255), random(100,255));
-      colour = (255,0, 255,0);
-      ellipse(x, y, d*2, d*2);
+      mag = pow(mag, 10);//sets the mag variable to power of 10
+      mag = sqrt(mag);//sets the square root of the magnitude
+      var magmax = sqrt(pow(10, 10));// gets a new maximum magnitude
+      var d = map(mag, 0, magmax, 0, 180);//sets the diameter in order to map the ellipses
+      stroke(255, 0, 255);//adds a stroke to the ellipses
+      fill(random(100,255), random(100,255), random(100,255)); // fills the ellipses with a random colour
+      //colour = (255,0, 255,0);
+      ellipse(x, y, d*2, d*2);// drawing the ellipses onto the canvas
 }}
 
 //tried incorparating a button which could change the colour of the data but it was messing with accessing my data and however couldn't incorparate it
